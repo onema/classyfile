790b5935fc963ab3130842ef2c9730e3152cedc1
Juan Manuel Torres <kinojman@gmail.com>
2015-05-26 00:13:39 -0700

    Method has been moved to generateClasses. This method returns an array organized by [original_file_name][ClassName]. dispatching events has been fixed. Saving classes to files to local file system by default, but other file systems may be used.

--- src/ClassyFile.php
+++ src/ClassyFile.php
@@ -7,7 +7,12 @@
  */
 namespace Onema\ClassyFile;
 
-use Oneme\ClassyFile\Exception\ClassToFileRuntimeException;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\AdapterInterface;
+use League\Flysystem\Filesystem;
+use Onema\ClassyFile\Exception\MissingEventDispatcherException;
+use Onema\ClassyFile\Plugin\GenerateClassFile;
+use Onema\ClassyFile\Exception\ClassToFileRuntimeException;
 use PhpParser\Error;
 use PhpParser\Lexer;
 use PhpParser\Node\Stmt\Class_;
@@ -37,9 +42,30 @@ class ClassyFile
      */
     protected $dispatcher;
 
+    /**
+     * @var \League\Flysystem\AdapterInterface
+     */
+    protected $filesystemAdapter;
+
+    /**
+     * @var callable
+     */
+    private $template;
+
     public function __construct()
     {
         $this->prettyPrinter = new Standard();
+        $this->setTemplate(function ($namespace, $uses, $comments, $code) {
+            return '<?php'.
+            PHP_EOL.PHP_EOL.
+            $namespace .
+            PHP_EOL.
+            $uses.
+            PHP_EOL.
+            $comments.
+            PHP_EOL.
+            $code.
+            PHP_EOL;});
     }
 
     /**
@@ -59,21 +85,58 @@ class ClassyFile
     }
 
     /**
-     * @param $directoryPath
+     * @param \League\Flysystem\AdapterInterface
+     */
+    public function setFilesystemAdapter(AdapterInterface $filesystemAdapter)
+    {
+        $this->filesystemAdapter = $filesystemAdapter;
+    }
+
+    /**
+     * This method is just a wrapper to the generateClasses method. It will add an event subscriber that will
+     * save the file to the desired location. The file uses a local file system addapter, but any file system may be
+     * used, this way files can be saved to remote locations.
+     *
+     * @param $codeDestination
+     * @param $codeLocation
      * @param bool $createNamespace
      * @param int $offset
      * @param int $length
      */
-    public function generateClassFiles($directoryPath, $createNamespace = false, $offset = 0, $length = 1)
+    public function generateClassFiles($codeDestination, $codeLocation, $createNamespace = false, $offset = 0, $length = 1)
+    {
+        if (!isset($this->dispatcher)) {
+            throw new MissingEventDispatcherException('This operation requires an instance of "Symfony\Component\EventDispatcher\EventDispatcherInterface".');
+        }
+
+        // add default local file system adapter.
+        if (!isset($this->filesystemAdapter)) {
+            $this->filesystemAdapter = new Local($codeDestination);
+        }
+
+        $filesystem = new Filesystem($this->filesystemAdapter);
+        $this->dispatcher->addSubscriber(new GenerateClassFile($filesystem));
+        $this->generateClasses($codeLocation, $createNamespace, $offset, $length);
+    }
+
+    /**
+     * @param $directoryPath
+     * @param bool $createNamespace
+     * @param int $offset
+     * @param int $length
+     * @returns array $code
+     */
+    public function generateClasses($directoryPath, $createNamespace = false, $offset = 0, $length = 1)
     {
         // Get all the files in the given directory
         $files = array_diff(scandir($directoryPath), array('..', '.'));
         $parser = new Parser(new Lexer());
+        $code = [];
 
         foreach ($files as $file) {
             $classes = file_get_contents(sprintf('%s/%s', $directoryPath, $file));
             $parts = pathinfo($file);
-            if ($parts['extension'] === 'php') {
+            if (isset($parts['extension']) && $parts['extension'] === 'php') {
                 try {
 
                     $statements = $parser->parse($classes);
@@ -99,29 +162,34 @@ class ClassyFile
                         $length = $event->getArgument('length');
                     }
 
-                    $this->traverseStatements($statements, $namespace);
+                    $code[$file] = $this->traverseStatements($statements, $namespace);
 
                 } catch (Error $e) {
                     throw new ClassToFileRuntimeException(sprintf('Parse Error: %s', $e->getMessage()));
                 }
             }
         }
+
+        return $code;
     }
 
     /**
      * @param $statements
      * @param string $namespaceString
+     * @returns array $code
      */
     protected function traverseStatements($statements, $namespaceString = '')
     {
         $uses = '';
+        $code = [];
 
         foreach ($statements as $statement) {
 
             if ($statement instanceof Namespace_) {
 
                 $namespaceString = implode('\\', $statement->name->parts);
-                $this->traverseStatements($statement->stmts, $namespaceString);
+                $nestedCode = $this->traverseStatements($statement->stmts, $namespaceString);
+                $code = array_merge($code, $nestedCode);
 
             } elseif ($statement instanceof Class_) {
 
@@ -129,18 +197,21 @@ class ClassyFile
                     $namespaceString = 'tmp';
                 }
 
-                $fileLocation = getcwd() . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $namespaceString);
-                $this->setClass($statement, $namespaceString, $fileLocation, $uses);
+                $fileLocation = DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $namespaceString);
+                $code[$statement->name] = $this->getClass($statement, $namespaceString, $fileLocation, $uses);
 
             } elseif ($statement instanceof If_) {
 
-                $this->traverseStatements($statement->stmts, $namespaceString);
+                $nestedCode = $this->traverseStatements($statement->stmts, $namespaceString);
+                $code = array_merge($code, $nestedCode);
 
             } elseif ($statement instanceof Use_) {
 
                 $uses .= $this->prettyPrinter->pStmt_Use($statement).PHP_EOL;
             }
         }
+
+        return $code;
     }
 
     /**
@@ -148,8 +219,9 @@ class ClassyFile
      * @param string $namespace
      * @param string $fileLocation
      * @param string $uses
+     * @returns string $code
      */
-    protected function setClass(Class_ $statement, $namespace, $fileLocation, $uses = '')
+    protected function getClass(Class_ $statement, $namespace, $fileLocation, $uses = '')
     {
         $comments = $statement->getDocComment();
         $comments = isset($comments) ? $comments->getText() : '';
@@ -161,38 +233,33 @@ class ClassyFile
         }
 
         if ($this->dispatcher) {
-            $event = $this->dispatch(ClassyFileEvent::SET_CLASS, [
+            $event = $this->dispatch(ClassyFileEvent::GET_CLASS, [
                 'statement' => $statement,
                 'namespace' => $namespace,
                 'file_location' => $fileLocation,
                 'uses' => $uses
             ]);
             $namespace = $event->getArgument('namespace');
-            $fileLocation = $event->getArgument('file_location');
             $uses = $event->getArgument('uses');
             $statement = $event->getStatement();
         }
 
         $code = $this->prettyPrinter->pStmt_Class($statement);
-        $code =
-            '<?php'.
-            PHP_EOL.PHP_EOL.
-            $namespace .
-            PHP_EOL.
-            $uses.
-            PHP_EOL.
-            $comments.
-            PHP_EOL.
-            $code.
-            PHP_EOL;
-        $name = $statement->name;
 
-        if (!is_dir($fileLocation)) {
-            // dir doesn't exist, make it
-            mkdir($fileLocation, 0777, true);
+        if (isset($this->template)) {
+            $code = call_user_func($this->template, $namespace, $uses, $comments, $code);
         }
 
-        file_put_contents(sprintf('%s/%s.php', $fileLocation, $name), $code);
+        if ($this->dispatcher) {
+            $event = $this->dispatch(ClassyFileEvent::AFTER_GET_CLASS, [
+                'code' => $code,
+                'statement' => $statement,
+                'file_location' => $fileLocation,
+            ]);
+            $code = $event->getArgument('code');
+        }
+
+        return $code;
     }
 
     protected function dispatch($eventName, array $arguments = [])
@@ -200,5 +267,13 @@ class ClassyFile
         $event = new ClassyFileEvent(null, $arguments);
         $this->dispatcher->dispatch($eventName, $event);
         return $event;
+    }
+
+    /**
+     * @param callable $template
+     */
+    public function setTemplate($template)
+    {
+        $this->template = $template;
     }
 }
