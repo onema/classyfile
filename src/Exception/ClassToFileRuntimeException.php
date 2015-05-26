d9a5996269d16d79c3db74273f05f89304b4ba51
Juan Manuel Torres <kinojman@gmail.com>
2015-05-26 00:07:44 -0700

    fixed typo

--- src/Exception/ClassToFileRuntimeException.php
+++ src/Exception/ClassToFileRuntimeException.php
@@ -5,7 +5,7 @@
  * please view the LICENSE file that was distributed 
  * with this source code.
  */
-namespace Oneme\ClassyFile\Exception;
+namespace Onema\ClassyFile\Exception;
 
 /**
  * ClassToFileRuntimeException - Description. 
