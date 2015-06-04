380e0339102e941c89b4bf3f41e87be86c2054b6
Juan Manuel Torres <kinojman@gmail.com>
2015-06-04 09:58:49 -0700

    code style update

--- src/Exception/ClassToFileRuntimeException.php
+++ src/Exception/ClassToFileRuntimeException.php
@@ -1,16 +1,19 @@
 <?php
 /*
  * This file is part of the Onema ClassyFile Package.
- * For the full copyright and license information, 
- * please view the LICENSE file that was distributed 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed
  * with this source code.
  */
+
 namespace Onema\ClassyFile\Exception;
 
 /**
- * ClassToFileRuntimeException - Description. 
+ * ClassToFileRuntimeException - Description.
  *
  * @author Juan Manuel Torres <kinojman@gmail.com>
  * @copyright (c) 2015, Onema
  */
-class ClassToFileRuntimeException extends \RuntimeException {}
+class ClassToFileRuntimeException extends \RuntimeException
+{
+}
