0fd2a771142a88006d146f51a9d3ce4b7f5631d9
Juan Manuel Torres <kinojman@gmail.com>
2015-06-03 22:12:50 -0700

    Moved constants from ClassyFileEvent and renamed them.

--- src/Event/TraverseEvent.php
+++ src/Event/TraverseEvent.php
@@ -1,20 +1,23 @@
 <?php
 /*
  * This file is part of the Onema ClassyFile Package.
- * For the full copyright and license information, 
- * please view the LICENSE file that was distributed 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed
  * with this source code.
  */
+
 namespace Onema\ClassyFile\Event;
 
 /**
- * GetClassEvent - Description. 
+ * GetClassEvent - Description.
  *
  * @author Juan Manuel Torres <kinojman@gmail.com>
  * @copyright (c) 2015, onema.io
  */
 class TraverseEvent extends ClassyFileEvent
 {
+    const BEFORE = 'classyfile.before_traverse';
+
     /**
      * @var
      */
