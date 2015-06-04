c60f1df962fc719c6f7ca5706286ef2c0d5fe417
Juan Manuel Torres <kinojman@gmail.com>
2015-06-03 22:14:10 -0700

    New event class, will contain file finder (BEFORE event) and generated classes array (AFTER event).

---
+++ src/Event/GenerateClassesEvent.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * This file is part of the Onema ClassyFile Package.
+ * For the full copyright and license information, 
+ * please view the LICENSE file that was distributed 
+ * with this source code.
+ */
+
+namespace Onema\ClassyFile\Event;
+
+use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * GenerateClassesEvent - Description. 
+ *
+ * @author Juan Manuel Torres <kinojman@gmail.com>
+ * @copyright (c) 2015, onema.io
+ */
+class GenerateClassesEvent extends Event
+{
+    const BEFORE = 'classyfile.before_generate_classes';
+    const AFTER = 'classyfile.after_generate_classes';
+
+    /**
+     * @var \Symfony\Component\Finder\Finder
+     */
+    private $finder;
+    /**
+     * @var array
+     */
+    private $classes;
+
+    public function __construct(Finder $finder)
+    {
+        $this->finder = $finder;
+    }
+
+    /**
+     * @return \Symfony\Component\Finder\Finder
+     */
+    public function getFinder()
+    {
+        return $this->finder;
+    }
+
+    /**
+     * @param \Symfony\Component\Finder\Finder $finder
+     */
+    public function setFinder($finder)
+    {
+        $this->finder = $finder;
+    }
+
+    /**
+     * @return array
+     */
+    public function getClasses()
+    {
+        return $this->classes;
+    }
+
+    /**
+     * @param array $classes
+     */
+    public function setClasses(array $classes)
+    {
+        $this->classes = $classes;
+    }
+}
