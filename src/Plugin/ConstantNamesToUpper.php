f4808408e7cf648f90d235636bc6318919403f90
Juan Manuel Torres <kinojman@gmail.com>
2015-06-04 09:57:12 -0700

    plugin can handle more constant name variations

--- src/Plugin/ConstantNamesToUpper.php
+++ src/Plugin/ConstantNamesToUpper.php
@@ -10,6 +10,7 @@ namespace Onema\ClassyFile\Plugin;
 
 use Onema\ClassyFile\Event\ClassyFileEvent;
 use Onema\ClassyFile\Event\GetClassEvent;
+use PhpParser\Node\Const_;
 use PhpParser\Node\Stmt\Class_;
 use PhpParser\Node\Stmt\ClassConst;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -47,7 +48,7 @@ class ConstantNamesToUpper implements EventSubscriberInterface
             for ($i = 0; $i < $count; $i++) {
                 if (is_array($statements->stmts)) {
                     $statement = $statements->stmts[$i];
-                    $this->toUpper($statement);
+                    $this->checkClassConstant($statement);
                     $statements->stmts[$i] = $statement;
                 }
             }
@@ -56,18 +57,35 @@ class ConstantNamesToUpper implements EventSubscriberInterface
         $event->setStatements($statements);
     }
 
-    private function toUpper($statement)
+    /**
+     * Converts class constant names to uppercase separated by underscores. Ignores all uppercase constants.
+     *
+     * @param $statement
+     * @return mixed
+     */
+    private function checkClassConstant($statement)
     {
         if ($statement instanceof ClassConst) {
             $countConst = count($statement->consts);
             for ($j = 0; $j < $countConst; $j++) {
-                $constant = $statement->consts[$j];
-                $name = strtoupper(preg_replace('/([^A-Z])([A-Z])/', '$1_$2', $constant->name));
-                $constant->name = str_replace('__', '_', $name);
-                $statement->consts[$j] = $constant;
+                $this->toUpper($statement->consts[$j]);
             }
         }
 
         return $statement;
+    }
+
+    private function toUpper(Const_ $constant)
+    {
+        if (strtoupper($constant->name) !== $constant->name) {
+            preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $constant->name, $matches);
+            $ret = $matches[0];
+            foreach ($ret as &$match) {
+                $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+            }
+            $constant->name = strtoupper(implode('_', $ret));
+        }
+
+        return $constant;
     }
 }
