3171d25225228ebf4a3eb81e74d0edb52e9259aa
Juan Manuel Torres <kinojman@gmail.com>
2015-06-05 00:13:24 -0700

    Adding new exception

---
+++ src/Exception/InvalidTemplateException.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of the Onema {classyfile} Package. 
+ * For the full copyright and license information, 
+ * please view the LICENSE file that was distributed 
+ * with this source code.
+ */
+namespace Onema\ClassyFile\Exception;
+
+/**
+ * InvalidTemplateException - Description. 
+ *
+ * @author Juan Manuel Torres <kinojman@gmail.com>
+ * @copyright (c) 2015, onema.io
+ */
+class InvalidTemplateException extends \Exception
+{
+}
