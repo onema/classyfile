22f612a286e8495d264e537d35befd581e519c25
Juan Manuel Torres <kinojman@gmail.com>
2015-06-06 10:24:34 -0700

    Update comments.

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * This file is part of the Onema {classyfile} Package. 
+ * This file is part of the Onema ClassyFile Package.
  * For the full copyright and license information, 
  * please view the LICENSE file that was distributed 
  * with this source code.
